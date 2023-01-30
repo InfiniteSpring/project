41ed3e79b8d74571aeea5fed6a7cc31779bd41e1
iden143 <iden143@mail.ru>
2023-01-30 14:18:00 +0300

    .

--- firstpart/main/urls.py
+++ firstpart/main/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.homepage, name="homepage"),
-    path('register', views.register_request, name='register')
+    path('register/', views.register_request, name='register'),
+    path('login/', views.login_request, name='login')
 ]
