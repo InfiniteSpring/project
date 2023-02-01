c7343a8f18b8703c0277ceb658e6da25985b5ed8
iden143 <iden143@mail.ru>
2023-02-01 17:49:58 +0300

    .

--- firstpart/firstpart/urls.py
+++ firstpart/firstpart/urls.py
@@ -3,6 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),
-    path('orders/', include('orders.urls'))
+    path('', include('main.urls')),    
 ]
--- firstpart/main/models.py
+++ firstpart/main/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class Orders(models.Model):
 
     name = models.CharField('Имя клиента', max_length= 50, default= '')
-    address = models.CharField('Адресс', min_lenth= 3, max_lenth= 50)
-    employee = models.CharField('Имя сотрудника', max_lenth= 100, default= '')
-    equipment = models.CharField('Оборудование', max_lenth = 45, defuult= '')
+    address = models.CharField('Адресс', max_length= 50)
+    employee = models.CharField('Имя сотрудника', max_length= 100, default= '')
+    equipment = models.CharField('Оборудование', max_length = 45, default= '')
     breaking = models.CharField('Поломка', max_length= 45, default= '')
     status = models.CharField('Статус', max_length= 45, default= '')
     date = models.DateField('Дата')
