513cd4947e882298e60dabf0143c9b187bd62e4f
EternalSpring <nikitaloverov04@mail.ru>
2023-02-17 01:12:45 +0300

    register

--- drholod/core/forms.py
+++ drholod/core/forms.py
@@ -9,10 +9,10 @@ class CustomUserCreationForm(UserCreationForm):
     groups = forms.ModelChoiceField(queryset=Group.objects.all())
     
     def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs)
-    for visible in self.visible_fields():
-        visible.field.widget.attrs['class'] = 'form-control'
-        visible.field.widget.attrs['placeholder'] = visible.field.label
+        super().__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+            visible.field.widget.attrs['placeholder'] = visible.field.label
     
     class Meta:
         model = CustomUser
