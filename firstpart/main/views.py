5cd64a440c2510cfe90ede1e0cd4fbbd44dee1be
EternalSpring <nikitaloverov04@mail.ru>
2023-02-17 15:16:21 +0300

    login

--- firstpart/main/views.py
+++ firstpart/main/views.py
@@ -1,48 +1,48 @@
+#10011101010100100010110101110010010101010111011101110001010
 from django.shortcuts import render, redirect
-from django.contrib.auth import views as auth_views
-from .forms import CustomUserCreationForm, LoginForm
-from django.contrib.auth import logout, login, authenticate
+from .forms import CustomUserCreationForm, UserLoginForm
+from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
-from django.contrib.auth.models import Group
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.contrib.auth.forms import AuthenticationForm
+from .models import Orders
 
-
-
-class SignUpView(CreateView):
-    form_class = CustomUserCreationForm
-    success_url = reverse_lazy('homepage')
-    template_name = 'main/signup.html'
-
-    def post(self, request, *args, **kwargs):
-        form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.save()
-            user_group = Group.objects.get(name=form.cleaned_data['groups'])
-            user.groups.add(user_group)          
-            logout(request)
-            new_user = authenticate(username=form.cleaned_data['username'],
-                                    password=form.cleaned_data['password1'],
-                                    )
-            login(request, new_user)
-
-            return redirect('homepage')
-        else:
-            return render(request, self.template_name, {'form' : form })
-
-class LoginView(auth_views.LoginView):
-    form_class = LoginForm
-    template_name = 'main/login.html'
-
-def orders_view(request):
-	return render(request, 'main/выполненные заказы.html')
 
 def homepage(request):
-	#info = Orders.object.all()
 	return render(request, 'main/homepage.html')
+
+def register_request(request):
+	if request.method == "POST":
+		form = CustomUserCreationForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request, user)
+			messages.success(request, "Регистрация успешна.")
+			return redirect("homepage")
+		messages.error(
+		    request, "Регистрация не была завершена. Неверно введена информация.")
+	form = CustomUserCreationForm()
+	return render(request=request, template_name="main/register.html", context={"register_form": form})
+
+
+def login_request(request):
+	if request.method == "POST":
+		form = AuthenticationForm(request, data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				login(request, user)
+				messages.info(request, f"Вы вошли как {username}.")
+				return redirect("homepage")
+			else:
+				messages.error(request, "Неправильное имя пользователя или пароль.")
+		else:
+			messages.error(request, "Неправильное имя пользователя или пароль.")
+	form = AuthenticationForm()
+	return render(request=request, template_name="main/login.html", context={"login_form": form})
 
 def logout_request(request):
 	logout(request)
 	messages.info(request, "Вы успешно вышли из аккаунта.") 
-	return redirect("homepage")	
+	return redirect("homepage")
