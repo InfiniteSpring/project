4996abb15548d1e1dae36db8d8df76f3b2b844f9
EternalSpring <nikitaloverov04@mail.ru>
2023-02-19 15:32:17 +0300

    homepage_3d

--- drholod/drholod/urls.py
+++ drholod/drholod/urls.py
@@ -5,6 +5,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
     path('', include('core.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) #при выгрузке на сервер
