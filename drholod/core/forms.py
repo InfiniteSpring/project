3d3570fa2abc4a502ca1b99d70b175d25ef75ecc
EternalSpring <nikitaloverov04@mail.ru>
2023-02-22 19:06:20 +0300

    sign/log final

--- drholod/core/forms.py
+++ drholod/core/forms.py
@@ -2,21 +2,50 @@ from django import forms
 from django.contrib.auth.models import Group
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
-from .models import CustomUser
 
+from .models import CustomUser
+ 
 
 class CustomUserCreationForm(UserCreationForm):
-    groups = forms.ModelChoiceField(queryset=Group.objects.all())
+    username = forms.CharField(label='')
+    email = forms.EmailField(label='')
+    password1 = forms.CharField(label='')
+    password2 = forms.CharField(label='')
+    groups = forms.ModelChoiceField(label='Должность', queryset=Group.objects.all())
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        count = 0
+        placeholders = ['Имя пользователя', 'Электронная почта', 'Пароль', 'Подтверждение пароля', 'Должность']
         for visible in self.visible_fields():
             visible.field.widget.attrs['class'] = 'form-control'
-            visible.field.widget.attrs['placeholder'] = visible.field.label
-    
+            visible.field.widget.attrs['placeholder'] = placeholders[count]
+            count += 1
+
+
     class Meta:
         model = CustomUser
-        fields = ('username', 'fio', 'email',)
+        fields = ('username', 'email',)
+
+        widgets = {
+        'placeholder' : {
+            'username': 'Элетронная почта'
+        }
+    }
+
+
+        
+
 
 class LoginForm(AuthenticationForm):
-    username = forms.CharField(label='Электронная почта')    
+    username = forms.CharField(label='')  
+    password = forms.CharField(label='')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        count = 0
+        placeholders = ['Электронная почта', 'Пароль']
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+            visible.field.widget.attrs['placeholder'] = placeholders[count]
+            count += 1
