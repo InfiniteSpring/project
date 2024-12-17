254d8a8249c654bed3f9d9d991151705f43b4a98
Unknown <unknown>
2024-12-17 13:28:36 +0300

    changes to App.jsx

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -10,11 +10,6 @@ function App() {
         <div className="only-computer-error">
         This site is designed for use on laptops and desktops
         </div>
-        <div className="only-computer-error-bottom">
-          <a href="https://github.com/grokify/grokify-website">
-            <img alt="github" width="32" height="32" />
-          </a>
-        </div>
       </div>
     )
   }
