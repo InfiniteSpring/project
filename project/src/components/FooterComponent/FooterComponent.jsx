fde2d2e0381ea67f8297a3400daf511128f086a6
InfiniteSpring <loverovnikita04@gmail.com>
2024-12-22 22:29:23 +0300

    finish sizing & rebuild header &rebuild footer & another color schema & little style fixes

--- project/src/components/FooterComponent/FooterComponent.jsx
+++ project/src/components/FooterComponent/FooterComponent.jsx
@@ -21,153 +21,61 @@ export default function FooterComponent() {
 
 
     return (
-        <div>
-            <footer>
-                <div className="links">
-                    <div className="contact-container">
-                        <div className="make-order">
-                            <h2>Make an order</h2>
-                            <p>We are ready to help you with your project</p>
-                            <form className="send-order-form">
-                                <div className="inputs">
-                                    <input
-                                        type="text"
-                                        name="name"
-                                        id="name"
-                                        placeholder="Name"
-                                        onChange={changeName}
-                                        value={isName}
-                                    /><br/>
-                                    <input
-                                        type="text"
-                                        name="email"
-                                        id="email"
-                                        placeholder="Email"
-                                        onChange={changeEmail}
-                                        value={isEmail}
-                                    /><br/>
-                                    <input
-                                        type="text"
-                                        name="subject"
-                                        id="subject"
-                                        placeholder="Subject"
-                                        onChange={changeSubject}
-                                        value={isSubject}
-                                    /><br/> 
-                                </div>
-                                <p>Just send an order and we will contact you soon</p>
-                                <button type="submit">Send order</button>
-                            </form>
+        <footer>
+            {/* <div className="upper">
+                <div className="upper-upper">
+
+                </div>
+                <div className="upper-middle">
+
+                </div>
+                <div className="upper-lower">
+
+                </div>
+            </div> */}
+            <div className="lower">
+                <div className="left-part">
+                    <div className="text">
+                        <div className="copy-year">
+                            &copy; 2025
                         </div>
-                    </div>
-                    <div className="container">
-                        <div className="services link-container-block">
-                            <div className="link-container-block-title">
-                            Services
-                            </div><br/>
-                            <div className="service-link">
-                            <a href="#">Software development</a>
-                            </div><br/>
-                            <div className="service-link">
-                            <a href="#">Analisys and promotion</a>
-                            </div><br/>
-                            <div className="service-link">
-                            <a href="#">Design</a>
-                            </div><br/>
-                            <div className="service-link">
-                            <a href="#">Quality assurance</a>
-                            </div><br/>
-                            <div className="service-link">
-                            <a href="#">Devops</a>
-                            </div><br/>
+                        <div className="text-under">
+                            Made by Proxity
                         </div>
-                        <div className="technologies link-container-block">
-                            <div className="link-container-block-title">
-                            Domains
-                            </div><br/>
-                            <div className="technologies-link">
-                            <a href="#">Web development</a>
-                            </div><br/>
-                            <div className="technologies-link">
-                            <a href="#">Big data</a>
-                            </div><br/>
-                            <div className="technologies-link">
-                            <a href="#">Data analysis</a>
-                            </div><br/>
-                            <div className="technologies-link">
-                            <a href="#">Machine learning</a>
-                            </div><br/>
-                            <div className="technologies-link">
-                            <a href="#">Artificial intelligence</a>
-                            </div><br/>
-                        </div>
-                        <div className="industries link-container-block">
-                            <div className="link-container-block-title">
-                            Industries
-                            </div><br/>
-                            <div className="industries-link">
-                            <a href="#">eCommerce</a>
-                            </div><br/>
-                            <div className="industries-link">
-                            <a href="#">Education</a>
-                            </div><br/>
-                            <div className="industries-link">
-                            <a href="#">Finance</a>
-                            </div><br/>
-                            <div className="industries-link">
-                            <a href="#">Logistics</a>
-                            </div><br/>
-                            <div className="industries-link">
-                            <a href="#">Healthcare</a>
-                            </div><br/>
-                        </div>
-                        <div className="about link-container-block">
-                            <div className="link-container-block-title">
-                            About
-                            </div><br/>
-                            <div className="about-link">
-                            <a href="#">Who we are</a>
-                            </div><br/>
-                            <div className="about-link">
-                            <a href="#">How we work</a>
-                            </div><br/>
-                            <div className="about-link">
-                            <a href="#">Technologies</a>
-                            </div><br/>
-                            <div className="about-link">
-                            <a href="#">Blog</a>
-                            </div><br/>
-                            <div className="about-link">
-                            <a href="#">Partnership</a>
-                            </div><br/>
-                        </div>
+
                     </div>
                 </div>
-                <div className="copy-contacts">
-                    <div className="copywrite">
-                        <div className="footer-logo">
-                        Proxity
-                        </div>
-                        <div className="copytext">
-                        &copy; 2024 Proxity. All Rights Reserved.
-                        </div>
-                    </div>
-                    <div className="contacts">
-                        <div className="email">
-                        proxity@gmail.com
-                        </div>
-                        <div className="xtwitter">
-                        @proxity Twitter
-                        </div>
+                <div className="middle-part">
+                    <div className="links">
                         <div className="linkedin">
-                        @proxity Linkedin
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
+                                <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z"/>
+                            </svg>
+                        </div>
+                        <div className="twitter">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter-x" viewBox="0 0 16 16">
+                                <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"/>
+                            </svg>
                         </div>
                         <div className="telegram">
-                        @proxity
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telegram" viewBox="0 0 16 16">
+                                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.287 5.906q-1.168.486-4.666 2.01-.567.225-.595.442c-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294q.39.01.868-.32 3.269-2.206 3.374-2.23c.05-.012.12-.026.166.016s.042.12.037.141c-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8 8 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629q.14.092.27.187c.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.4 1.4 0 0 0-.013-.315.34.34 0 0 0-.114-.217.53.53 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09"/>
+                            </svg>
+                        </div>
+                        <div className="reddit">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-reddit" viewBox="0 0 16 16">
+                                <path d="M6.167 8a.83.83 0 0 0-.83.83c0 .459.372.84.83.831a.831.831 0 0 0 0-1.661m1.843 3.647c.315 0 1.403-.038 1.976-.611a.23.23 0 0 0 0-.306.213.213 0 0 0-.306 0c-.353.363-1.126.487-1.67.487-.545 0-1.308-.124-1.671-.487a.213.213 0 0 0-.306 0 .213.213 0 0 0 0 .306c.564.563 1.652.61 1.977.61zm.992-2.807c0 .458.373.83.831.83s.83-.381.83-.83a.831.831 0 0 0-1.66 0z"/>
+                                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.828-1.165c-.315 0-.602.124-.812.325-.801-.573-1.9-.945-3.121-.993l.534-2.501 1.738.372a.83.83 0 1 0 .83-.869.83.83 0 0 0-.744.468l-1.938-.41a.2.2 0 0 0-.153.028.2.2 0 0 0-.086.134l-.592 2.788c-1.24.038-2.358.41-3.17.992-.21-.2-.496-.324-.81-.324a1.163 1.163 0 0 0-.478 2.224q-.03.17-.029.353c0 1.795 2.091 3.256 4.669 3.256s4.668-1.451 4.668-3.256c0-.114-.01-.238-.029-.353.401-.181.688-.592.688-1.069 0-.65-.525-1.165-1.165-1.165"/>
+                            </svg>
                         </div>
                     </div>
                 </div>
-            </footer>
-        </div>
+                <div className="right-part">
+                    <div className="text">
+                        Proxity
+                    </div>
+                </div>
+            </div>
+        </footer>
     );
 }   
--- project/src/components/HeaderComponent/HeaderComponent.jsx
+++ project/src/components/HeaderComponent/HeaderComponent.jsx
@@ -34,6 +34,13 @@ export default function HeaderComponent() {
                     <a href="/domains">domains</a>
                     <a href="/industries">industries</a>
                     {/* <a href="/portfolio">portfolio</a> */}
+                </div>
+                <div className="contacts">
+                    <a className="lng" href="/language">Global(EN)
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down" viewBox="0 0 16 16">
+                      <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1"/>
+                    </svg>
+                    </a>
                     <a className="contact-us-btn-1" href="/contact">contact us</a>
                 </div>
             </header>
